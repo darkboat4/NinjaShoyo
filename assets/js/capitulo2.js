c6247dffd85c2e9b471ac857e9488ba667098c90
darkboat4 <rssgamer2017@gmail.com>
2025-03-19 17:22:26 -0300

    primeiro commit

---
+++ assets/js/capitulo2.js
@@ -0,0 +1,4 @@
+import { Capitulo } from "./gerar_imgs.js";
+
+const cap1 = new Capitulo(2, 25);
+cap1.criar_cap();
---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function abrircap(){
+    window.location.href= "assets/pag/capitulo.html"
+}
+
+function proxcap(next){
+    window.location.href= `cap${next}.html`;
+}
+
+function voltarcap(prev){
+    window.location.href= `cap${prev}.html`;
+}
